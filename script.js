e6f27939b66564aa23ccb34e86e41811be965d0b
Chris Vazquez <cvg_v10@hotmail.com>
2022-03-01 21:30:13 -0800

    set up all the hours and storage

---
+++ script.js
@@ -0,0 +1,85 @@
+$(document).ready(function () {
+  $('#10text').val(localStorage.getItem('10text'))
+  $('#hour10').on("click", function () {
+    localStorage.setItem('10text', $('#10text').val())
+  })
+
+})
+$(document).ready(function () {
+  $('#11text').val(localStorage.getItem('11text'))
+  $('#hour11').on("click", function () {
+    localStorage.setItem('11text', $('#11text').val())
+  })
+
+})
+$(document).ready(function () {
+  $('#12text').val(localStorage.getItem('12text'))
+  $('#hour12').on("click", function () {
+    localStorage.setItem('12text', $('#12text').val())
+  })
+
+})
+$(document).ready(function () {
+  $('#1text').val(localStorage.getItem('1text'))
+  $('#hour1').on("click", function () {
+    localStorage.setItem('1text', $('#1text').val())
+  })
+
+})
+$(document).ready(function () {
+  $('#2text').val(localStorage.getItem('2text'))
+  $('#hour2').on("click", function () {
+    localStorage.setItem('2text', $('#2text').val())
+  })
+
+})
+$(document).ready(function () {
+  $('#3text').val(localStorage.getItem('3text'))
+  $('#hour3').on("click", function () {
+    localStorage.setItem('3text', $('#3text').val())
+  })
+
+})
+$(document).ready(function () {
+  $('#4text').val(localStorage.getItem('4text'))
+  $('#hour4').on("click", function () {
+    localStorage.setItem('4text', $('#4text').val())
+  })
+
+})
+$(document).ready(function () {
+  $('#5text').val(localStorage.getItem('5text'))
+  $('#hour5').on("click", function () {
+    localStorage.setItem('5text', $('#5text').val())
+  })
+})
+$(document).ready(function () {
+  $('#6text').val(localStorage.getItem('6text'))
+  $('#hour6').on("click", function () {
+    localStorage.setItem('6text', $('#6text').val())
+  })
+})
+$(document).ready(function () {
+  $('#7text').val(localStorage.getItem('7text'))
+  $('#hour7').on("click", function () {
+    localStorage.setItem('7text', $('#7text').val())
+  })
+})
+$(document).ready(function () {
+  $('#8text').val(localStorage.getItem('8text'))
+  $('#hour8').on("click", function () {
+    localStorage.setItem('8text', $('#8text').val())
+  })
+})
+$(document).ready(function () {
+  $('#9text').val(localStorage.getItem('9text'))
+  $('#hour9').on("click", function () {
+    localStorage.setItem('9text', $('#9text').val())
+  })
+})
+$(document).ready(function () {
+  $('#10ptext').val(localStorage.getItem('10ptext'))
+  $('#hour10p').on("click", function () {
+    localStorage.setItem('10ptext', $('#10ptext').val())
+  })
+})
